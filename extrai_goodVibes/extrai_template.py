4c9a7be8b85b6dbc778bcac55a5a0c295b6d8354
brittosandro <brittosandro@gmail.com>
2022-04-30 19:05:39 -0300

    criando função propriedades

--- extrai_goodVibes/extrai_template.py
+++ extrai_goodVibes/extrai_template.py
@@ -102,6 +102,51 @@ def dados_complexos(arquivo):
 
     return np.asarray(lista_dividida)
 
+def prop_fisicas(prop1, prop2, prop3):
+    '''
+    Essa função recebe três propriedades, que podem ser, por exemplo: entalpia
+    do Templete, entalpia do Monomero e entalpia do complexo, retornando a
+    variacao da entalpia (Delta H) em kcal/mol. Caso receba a energia de Gibbs,
+    tera: energias de Gibbs do Templete, energia de Gibbs do monomero e energia
+    de Gibbs do complexo, retornando a variacao da energia de Gibbs (Delta G)
+    em kcal/mol.
+
+    - prop1 = propriedade do monomero.
+    - prop2 = propriedade do templete.
+    - prop3 = propriedade do complexo.
+    '''
+    return (float(prop3) - (float(prop1) + float(prop2))) * 627.5
+
+def prop_fisicas2mono(prop1, prop2, prop3):
+    '''
+    Essa função recebe três propriedades, que podem ser, por exemplo: entalpia
+    do Templete, entalpia do Monomero e entalpia do complexo, retornando a
+    variacao da entalpia (Delta H) em kcal/mol. Caso receba a energia de Gibbs,
+    tera: energias de Gibbs do Templete, energia de Gibbs do monomero e energia
+    de Gibbs do complexo, retornando a variacao da energia de Gibbs (Delta G)
+    em kcal/mol. Mas calcula a propriedade considerando 2 monomeros.
+
+    - prop1 = propriedade do monomero.
+    - prop2 = propriedade do templete.
+    - prop3 = propriedade do complexo.
+    '''
+    return (float(prop3) - (2*float(prop1) + float(prop2))) * 627.51
+
+def prop_fisicas3mono(prop1, prop2, prop3):
+    '''
+    Essa função recebe três propriedades, que podem ser, por exemplo: entalpia
+    do Templete, entalpia do Monomero e entalpia do complexo, retornando a
+    variacao da entalpia (Delta H) em kcal/mol. Caso receba a energia de Gibbs,
+    tera: energias de Gibbs do Templete, energia de Gibbs do monomero e energia
+    de Gibbs do complexo, retornando a variacao da energia de Gibbs (Delta G)
+    em kcal/mol. Mas calcula a propriedade considerando 3 monomeros.
+
+    - prop1 = propriedade do monomero.
+    - prop2 = propriedade do templete.
+    - prop3 = propriedade do complexo.
+    '''
+    return (float(prop3) - (3*float(prop1) + float(prop2))) * 627.51
+
 def graf_entalpia_um_mono(int_temp, entalp_CBZ_acet_1ACR, entalp_CBZ_acet_1IA,
                          entalp_CBZ_acet_1MAA, entalp_CBZ_acet_1VP):
 
@@ -111,11 +156,11 @@ def graf_entalpia_um_mono(int_temp, entalp_CBZ_acet_1ACR, entalp_CBZ_acet_1IA,
     # Ajusta subplots.
     fig.subplots_adjust(
                           left = 0.100,
-                          right = 0.835,    # {Define as distâncias entre os extremos}
+                          right = 0.835,  # Define as distâncias entre os extremos
                           bottom = 0.14,
                           top = 0.890,
-                          hspace = 0.24,   # Organiza espaçoes entre os subplots
-                          wspace = 0.23    # Organiza espaçoes entre os subplots
+                          hspace = 0.24,  # Organiza espaçoes entre os subplots
+                          wspace = 0.23   # Organiza espaçoes entre os subplots
                        )
 
     fig1 = ax.plot(interval_temp, entalp_CBZ_acet_1ACR, color='k', linewidth=2.8, label='ACR')
@@ -149,10 +194,10 @@ def graf_entalpia_um_mono(int_temp, entalp_CBZ_acet_1ACR, entalp_CBZ_acet_1IA,
              fontsize = 'xx-large',
              rotation='vertical')      # Rotaciona o nome do eixo
 
-    #plt.savefig('HN_nmove_NVT.png', dpi=300, orientation='portrait', transparent=True, format='png')
+    plt.savefig('entalpia_um.png', dpi=300, orientation='portrait', transparent=True, format='png')
     plt.show()
 
-def graf_entalpia_dois_mono(interval_temp, entalp_CBZ_acet_2ACR, entalp_CBZ_acet_21IA,
+def graf_entalpia_dois_mono(interval_temp, entalp_CBZ_acet_2ACR, entalp_CBZ_acet_2IA,
                             entalp_CBZ_acet_2MAA, entalp_CBZ_acet_2VP):
 
     sns.set(style="ticks")
@@ -176,19 +221,19 @@ def graf_entalpia_dois_mono(interval_temp, entalp_CBZ_acet_2ACR, entalp_CBZ_acet
                frameon=False, title = "Dois Monômeros", prop = {'size':15})
     plt.grid(False)
 
-    fig.text(0.45,                     # Ordena Posição x
-             0.92,                    # Ordena Posição y
-             'CBZ em Acetonitrila',      # Texto A ser colocado
-             ha='center',            # Alinha texto horizontalmente
-             va='center',            # Alinha texto verticalmente
+    fig.text(0.45,                  # Ordena Posição x
+             0.92,                  # Ordena Posição y
+             'CBZ em Acetonitrila', # Texto A ser colocado
+             ha='center',           # Alinha texto horizontalmente
+             va='center',           # Alinha texto verticalmente
              fontsize = 'xx-large')
 
 
-    fig.text(0.45,                      # Ordena Posição x
-             0.05,                     # Ordena Posição y
-             'Temperatura (K)',     # Texto A ser colocado
-             ha='center',             # Alinha texto horizontalmente
-             va='center',             # Alinha texto verticalmente
+    fig.text(0.45,                   # Ordena Posição x
+             0.05,                   # Ordena Posição y
+             'Temperatura (K)',      # Texto A ser colocado
+             ha='center',            # Alinha texto horizontalmente
+             va='center',            # Alinha texto verticalmente
              fontsize = 'xx-large')
 
     fig.text(0.029,
@@ -199,10 +244,58 @@ def graf_entalpia_dois_mono(interval_temp, entalp_CBZ_acet_2ACR, entalp_CBZ_acet
              fontsize = 'xx-large',
              rotation='vertical')      # Rotaciona o nome do eixo
 
-    #plt.savefig('HN_nmove_NVT.png', dpi=300, orientation='portrait', transparent=True, format='png')
+    plt.savefig('entapia_2mono.png', dpi=300, orientation='portrait', transparent=True, format='png')
     plt.show()
 
+def graf_entalpia_tres_mono(interval_temp, entalp_CBZ_acet_3ACR, entalp_CBZ_acet_3IA,
+                            entalp_CBZ_acet_3MAA, entalp_CBZ_acet_3VP):
 
+    sns.set(style="ticks")
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Ajusta subplots.
+    fig.subplots_adjust(
+                          left = 0.105,
+                          right = 0.820,    # {Define as distâncias entre os extremos}
+                          bottom = 0.14,
+                          top = 0.890,
+                          hspace = 0.24,   # Organiza espaçoes entre os subplots
+                          wspace = 0.23    # Organiza espaçoes entre os subplots
+                       )
+
+    fig1 = ax.plot(interval_temp, entalp_CBZ_acet_3ACR, color='k', linewidth=2.8, label='ACR')
+    fig2 = ax.plot(interval_temp, entalp_CBZ_acet_3IA, color='b', linewidth=2.8, label='IA')
+    fig3 = ax.plot(interval_temp, entalp_CBZ_acet_3MAA, color='r', linewidth=2.8, label='MAA')
+    fig4 = ax.plot(interval_temp, entalp_CBZ_acet_3VP, color='y', linewidth=2.8, label='VP')
+    fig.legend(loc='upper right', shadow=False, fontsize='large', bbox_to_anchor=(1.01, 0.88),
+               frameon=False, title = "Três Monômeros", prop = {'size':15})
+    plt.grid(False)
+
+    fig.text(0.45,                  # Ordena Posição x
+             0.92,                  # Ordena Posição y
+             'CBZ em Acetonitrila', # Texto A ser colocado
+             ha='center',           # Alinha texto horizontalmente
+             va='center',           # Alinha texto verticalmente
+             fontsize = 'xx-large')
+
+
+    fig.text(0.45,                   # Ordena Posição x
+             0.05,                   # Ordena Posição y
+             'Temperatura (K)',      # Texto A ser colocado
+             ha='center',            # Alinha texto horizontalmente
+             va='center',            # Alinha texto verticalmente
+             fontsize = 'xx-large')
+
+    fig.text(0.029,
+             0.5,
+             '$\Delta H$ ($kcal/mol$)',
+             ha='center',
+             va='center',
+             fontsize = 'xx-large',
+             rotation='vertical')      # Rotaciona o nome do eixo
+
+    plt.savefig('entalpia_tres_mono.png', dpi=300, orientation='portrait', transparent=True, format='png')
+    plt.show()
 
 if __name__ == '__main__':
     nome_template = slice(2, 10)
@@ -242,46 +335,52 @@ if __name__ == '__main__':
                 for templete, monomero, complexo in zip(templetes, monomeros, complexos):
                     #print(f'{templete[nome_template]}  |  {monomero[nome_monomero]}  |  {complexo[nome_complexo]}')
                     if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'ACR_acet' and complexo[nome_complexo] == 'CBZ_one_ACR_acet':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_1ACR.append(entalpia_em_kcal_mol)
-                        #entalp_CBZ_acet_2ACR.append(float(templete[entalpia]) + 2*float(monomero[entalpia]))
-                        #entalp_CBZ_acet_3ACR.append(float(templete[entalpia]) + 3*float(monomero[entalpia]))
+                        v_entalpia = prop_fisicas(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_1ACR.append(v_entalpia)
 
                     if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'ACR_acet' and complexo[nome_complexo] == 'CBZ_two_ACR_acet':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + 2*float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_2ACR.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas2mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_2ACR.append(v_entalpia)
+
+                    if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'ACR_acet' and complexo[nome_complexo] == 'CBZ_three_ACR_ac':
+                        v_entalpia = prop_fisicas3mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_3ACR.append(v_entalpia)
 
                     if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'IA_aceto' and complexo[nome_complexo] == 'CBZ_one_IA_aceto':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_1IA.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_1IA.append(v_entalpia)
 
                     if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'IA_aceto' and complexo[nome_complexo] == 'CBZ_two_IA_aceto':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + 2*float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_2IA.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas2mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_2IA.append(v_entalpia)
+
+                    if templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'IA_aceto' and complexo[nome_complexo] == 'CBZ_three_IA_ace':
+                        v_entalpia = prop_fisicas3mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_3IA.append(v_entalpia)
 
                     if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'MAA_acet' and complexo[nome_complexo] == 'CBZ_one_MAA_acet':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_1MAA.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_1MAA.append(v_entalpia)
 
                     if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'MAA_acet' and complexo[nome_complexo] == 'CBZ_two_MAA_acet':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + 2*float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_2MAA.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas2mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_2MAA.append(v_entalpia)
+
+                    if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'MAA_acet' and complexo[nome_complexo] == 'CBZ_three_MAA_ac':
+                        v_entalpia = prop_fisicas3mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_3MAA.append(v_entalpia)
 
                     if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'VP_aceto' and complexo[nome_complexo] == 'CBZ_one_VP_aceto':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_1VP.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_1VP.append(v_entalpia)
 
                     if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'VP_aceto' and complexo[nome_complexo] == 'CBZ_two_VP_aceto':
-                        valor_entalpia = float(complexo[entalpia]) - (float(templete[entalpia]) + 2*float(monomero[entalpia]))
-                        entalpia_em_kcal_mol = valor_entalpia * 627.51
-                        entalp_CBZ_acet_2VP.append(entalpia_em_kcal_mol)
+                        v_entalpia = prop_fisicas2mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_2VP.append(v_entalpia)
+
+                    if  templete[nome_template] == 'CBZ_acet' and monomero[nome_monomero] == 'VP_aceto' and complexo[nome_complexo] == 'CBZ_three_VP_ace':
+                        v_entalpia = prop_fisicas3mono(monomero[entalpia], templete[entalpia], complexo[entalpia])
+                        entalp_CBZ_acet_3VP.append(v_entalpia)
 
 
 
@@ -298,31 +397,5 @@ if __name__ == '__main__':
     graf_entalpia_dois_mono(interval_temp, entalp_CBZ_acet_2ACR, entalp_CBZ_acet_2IA,
                           entalp_CBZ_acet_2MAA, entalp_CBZ_acet_2VP)
 
-
-
-
-
-    #print(s_entalp_CBZ_acet_2ACR)
-    #print(s_entalp_CBZ_acet_3ACR)
-    #print('-----')
-    #print(s_entalp_CBZ_acet_1IA)
-    #print(s_entalp_CBZ_acet_2IA)
-    #print(s_entalp_CBZ_acet_3IA)
-
-
-    #for monomeros in dados_monomeros:
-    #    print(monomeros)
-
-    #for d_templetes, d_monomeros in zip(dados_templetes('dados_templetes_output.dat'),
-    #                                    dados_monomeros('dados_monomero_output.dat')):
-        #print(d_templetes, d_monomeros)
-
-        #for dado_temple, dado_mono in zip(d_templetes, d_monomeros):
-        #    print(dado_temple[entalpia], dado_mono[entalpia])
-
-        #l = np.concatenate((d_templetes, d_monomeros))
-        #print(np.concatenate((d_templetes, d_monomeros)))
-        #print(l)
-        #for solv in l:
-        #    print(solv)
-    #print(l)
+    graf_entalpia_tres_mono(interval_temp, entalp_CBZ_acet_3ACR, entalp_CBZ_acet_3IA,
+                          entalp_CBZ_acet_3MAA, entalp_CBZ_acet_3VP)
